4d2da575c676fc84a1176f044814a44b78d5cce9
katsumag <goude47@gmail.com>
2020-07-04 14:02:12 +0100

    Update build.gradle

--- src/main/kotlin/me/katsumag/bedrockbases/BedrockBases.kt
+++ src/main/kotlin/me/katsumag/bedrockbases/BedrockBases.kt
@@ -1,6 +1,8 @@
 package me.katsumag.bedrockbases
 
+import me.katsumag.bedrockbases.commands.BaseCommand
 import me.katsumag.bedrockbases.world.VoidWordGenerator
+import me.mattstudios.mf.base.CommandManager
 import org.bukkit.WorldCreator
 import org.bukkit.generator.ChunkGenerator
 import org.bukkit.plugin.java.JavaPlugin
@@ -8,9 +10,9 @@ import org.bukkit.plugin.java.JavaPlugin
 class BedrockBases : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
-
+        val commandManager = CommandManager(this)
+        commandManager.register(BaseCommand(this))
         server.getWorld("BedrockBases") ?: createWorld()
-
     }
 
     override fun onDisable() {
